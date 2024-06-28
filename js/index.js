a40375100149900e9fe1cef4a84e29aea548b05f
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-06-28 22:36:27 +0530

    logic done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-console.log("Jai Shree Ram");
+const emailInput = document.querySelector(
+  ".inputContainer input[type='email']"
+);
+const verifyBtn = document.querySelector(".inputContainer button");
+const result = document.querySelector(".results");
+
+// Please do not use my API key. Create your own API key for production use.
+api_key = "ema_live_d756OCviZztsolHhmBOV0Ot2I9DcFIxHgqUTie8k";
+
+verifyBtn.addEventListener("click", async () => {
+  result.innerHTML += `<img width="123" src="../assets/loading.svg" alt="">`;
+  const email = emailInput.value;
+  const response = await fetch(
+    `https://api.emailvalidation.io/v1/info?apikey=${api_key}&email=${email}`
+  );
+  const jsonResponse = await response.json();
+  let stringInfo = "<h3>Results</h3>";
+  for (key of Object.keys(jsonResponse)) {
+    if (jsonResponse[key] !== "" && jsonResponse[key] !== " ") {
+      stringInfo += `<div><span>${key}</span> : ${jsonResponse[key]}</div>`;
+    }
+  }
+  emailInput.value = "";
+  result.innerHTML = stringInfo;
+});
